e9007e71c15338c3ff958b62a8d6eaed2b4ce326
Sky Brewer <jaco.brewer@gmail.com>
2022-09-28 16:48:50 +0200

    player main methods

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -1,12 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: usize = 80;
     pub const SCREEN_HEIGHT: usize = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;

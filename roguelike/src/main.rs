59e0a64e0e9a9fb089f95491fc46b965bd9d956b
Sky Brewer <jaco.brewer@gmail.com>
2022-09-29 11:58:03 +0200

    use graphics font

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -8,6 +8,8 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: usize = 80;
     pub const SCREEN_HEIGHT: usize = 50;
+    pub const DISPLAY_WIDTH: usize = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: usize = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -41,9 +43,14 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("Alien Cake")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_resource_path("assets/images/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())

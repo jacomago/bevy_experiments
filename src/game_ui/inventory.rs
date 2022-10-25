9e3ed0801eb9eb2852a7ae1c15a503a68a5799b4
Sky Brewer <jaco.brewer@gmail.com>
2022-10-25 14:01:22 +0200

    got a list working

--- src/game_ui/inventory.rs
+++ src/game_ui/inventory.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use winit::dpi::Position;
 
 use crate::{
     components::{carried::Carried, name::CharacterName},
@@ -37,17 +38,20 @@ pub fn spawn_inventory(mut commands: Commands, font: Res<FontAssets>) {
                     margin: UiRect {
                         left: Val::Auto,
                         right: Val::Auto,
+                        top: Val::Auto,
                         ..default()
                     },
                     ..default()
                 }),
             );
             parent
-                .spawn_bundle(TextBundle {
-                    text: Text {
-                        sections: vec![],
-                        alignment: TextAlignment::CENTER,
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::ColumnReverse,
+                        justify_content: JustifyContent::Center,
+                        ..default()
                     },
+                    color: UiColor(Color::rgba(0.65, 0.65, 0.65, 0.5)),
                     ..default()
                 })
                 .insert(Inventory);
@@ -58,23 +62,35 @@ pub fn spawn_inventory(mut commands: Commands, font: Res<FontAssets>) {
 pub struct Inventory;
 
 pub fn update_inventory(
+    mut commands: Commands,
     player_items: Query<(With<Carried>, &CharacterName)>,
-    mut text_query: Query<(&mut Text, With<Inventory>)>,
+    inventory_query: Query<(Entity, With<Inventory>)>,
     font: Res<FontAssets>,
 ) {
-    let (mut text, _) = text_query.single_mut();
-    text.sections = player_items
-        .iter()
-        .map(|(_, name)| {
-            info!("Adding {}", name.0);
-            TextSection {
-                value: name.0.clone(),
-                style: TextStyle {
-                    font: font.fira_sans.clone(),
-                    font_size: 25.,
-                    color: Color::BLACK,
-                },
-            }
-        })
-        .collect();
+    let (inventory, _) = inventory_query.single();
+    commands.entity(inventory).despawn_descendants();
+    commands.entity(inventory).with_children(|parent| {
+        player_items.iter().for_each(|(_, name)| {
+            parent.spawn_bundle(
+                TextBundle::from_section(
+                    name.0.clone(),
+                    TextStyle {
+                        font: font.fira_sans.clone(),
+                        font_size: 10.,
+                        color: Color::BLACK,
+                    },
+                )
+                .with_style(Style {
+                    size: Size::new(Val::Undefined, Val::Px(25.)),
+                    margin: UiRect {
+                        left: Val::Auto,
+                        right: Val::Auto,
+                        top: Val::Auto,
+                        ..default()
+                    },
+                    ..default()
+                }),
+            );
+        });
+    });
 }

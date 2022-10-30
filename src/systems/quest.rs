8675ed6d62dd322f346a3d9242f5a9c763baa0c9
Sky Brewer <jaco.brewer@gmail.com>
2022-10-30 15:56:50 +0100

    basic quest plugin

--- src/systems/quest.rs
+++ src/systems/quest.rs
@@ -1,9 +1,32 @@
 use bevy::prelude::*;
 
-use crate::config::ItemType;
+use crate::{config::ItemType, entities::Player};
+
+use super::inventory::Carried;
 
 #[derive(Debug, Component, Default, Clone, Copy)]
 pub struct Quest {
     pub giver: Option<Entity>,
     pub requested_item: ItemType,
+}
+
+pub struct RecieveQuest {}
+
+pub struct QuestPlugin;
+
+impl Plugin for QuestPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<RecieveQuest>();
+    }
+}
+
+pub fn quest(
+    mut commands: Commands,
+    mut quest_events: EventReader<RecieveQuest>,
+    player_query: Query<(Entity, With<Player>)>,
+) {
+    let (player, _) = player_query.single();
+    quest_events.iter().for_each(|event| {
+        info!("Recieve quest");
+    });
 }
